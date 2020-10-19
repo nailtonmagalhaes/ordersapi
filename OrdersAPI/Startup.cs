5c8a31764314f41328ab969c1e10e1397daa08d4
Nailton Magalhaes <nailton85@gmail.com>
2020-10-19 16:04:43 -0300

    Added  Swagger

--- OrdersAPI/Startup.cs
+++ OrdersAPI/Startup.cs
@@ -7,7 +7,9 @@ using OrdersAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using OrdersAPI.Repositories;
 using OrdersAPI.Repositories.Interfaces;
-using Microsoft.AspNetCore.Server.IISIntegration;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.Swagger;
+using System.IO;
 
 namespace OrdersAPI
 {
@@ -19,9 +21,31 @@ namespace OrdersAPI
         }
 
         public IConfiguration Configuration { get; }
+        public object PlatformServices { get; private set; }
 
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("All", builder =>
+                 {
+                     builder.AllowAnyOrigin();
+                     builder.AllowAnyMethod();
+                     builder.AllowAnyHeader();
+                 });
+            });
+            // services.AddCors(options =>
+            // {
+            //     options.AddPolicy(
+            //       "CorsPolicy",
+            //       builder => builder.WithOrigins("http://localhost:4200")
+            //                         .AllowAnyMethod()
+            //                         .AllowAnyHeader()
+            //                         .AllowCredentials());
+            // });
+            // services.AddAuthentication(IISDefaults.AuthenticationScheme);
+
             services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("DbOrdersControl"));
             //services.AddScoped<DataContext, DataContext>();
 
@@ -36,26 +60,48 @@ namespace OrdersAPI
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             );
 
-            services.AddResponseCompression();
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1",
+            //        new Microsoft.OpenApi.Models.OpenApiInfo
+            //        {
+            //            Title = "Conversor de Temperaturas",
+            //            Version = "v1",
+            //            Description = "Exemplo de API REST criada com o ASP.NET Core",
+            //            Contact = new Microsoft.OpenApi.Models.OpenApiContact
+            //            {
+            //                Name = "Renato Groffe",
+            //                Url = new System.Uri("https://github.com/renatogroffe")
+            //            }
+            //        });
 
-            services.AddCors(options => options.AddDefaultPolicy(builder =>
+            //    string caminhoAplicacao =
+            //        PlatformServices.Default.Application.ApplicationBasePath;
+            //    string nomeAplicacao =
+            //        PlatformServices.Default.Application.ApplicationName;
+            //    string caminhoXmlDoc =
+            //        Path.Combine(caminhoAplicacao, $"{nomeAplicacao}.xml");
+
+            //    c.IncludeXmlComments(caminhoXmlDoc);
+            //});
+
+            services.AddSwaggerGen(options =>
             {
-                builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
-            }));
-            // services.AddCors(options =>
-            // {
-            //     options.AddPolicy(
-            //       "CorsPolicy",
-            //       builder => builder.WithOrigins("http://localhost:4200")
-            //                         .AllowAnyMethod()
-            //                         .AllowAnyHeader()
-            //                         .AllowCredentials());
-            // });
-            // services.AddAuthentication(IISDefaults.AuthenticationScheme);
+                options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Place Info Service API",
+                    Version = "v2",
+                    Description = "Sample service for Learner",
+                });
+            });
+
+            services.AddResponseCompression();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("All");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -74,12 +120,8 @@ namespace OrdersAPI
 
             app.UseResponseCompression();
 
-            app.UseCors(options =>
-            {
-                options.AllowAnyOrigin();
-                options.AllowAnyMethod();
-                options.AllowAnyHeader();
-            });
+            app.UseSwagger();
+            app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v2/swagger.json", "PlaceInfo Services"));
 
             app.ApplicationServices.CreateScope().ServiceProvider.GetService<DataContext>().Database.EnsureCreated();
         }
